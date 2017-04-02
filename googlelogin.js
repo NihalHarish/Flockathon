c41b5fbb2cd40b569047fd9e64446044439cda0b
Elita Lobo <elita.l@endurance.com>
2017-04-02 09:15:37 +0530

    Added wit.ai

--- googlelogin.js
+++ googlelogin.js
@@ -5,6 +5,11 @@ var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
 
+const {Wit, log} = require('node-wit');
+
+var ACCESS_TOKEN = "KXKZYJIHXFSMSGLFCEIG77YP6FESIDLA";
+var timestamp = require("internet-timestamp")
+
 // Configure app to be a side bar app
 // Build UI for sidebar register endpoint for it i.e typeahead
 // Make a map between the Google API and the natural language input used for it.
@@ -88,16 +93,18 @@ app.post('/flock', function(req,res){
 app.get('/googleredirect', function(req,res){
   var code = req.query.code;
   var searchstring = req.query.searchstring;
+   console.log('searchstring : ' + searchstring);
   oauth2Client.getToken(code, function (err, tokens) {
   // Now tokens contains an access_token and an optional refresh_token. Save them.
   if (!err) {
     console.log(tokens)
     oauth2Client.setCredentials(tokens);
-    listFiles(oauth2Client);
-    res.send({success:true});
+    listFiles(oauth2Client,searchstring,res);
+    //res.send({success:true});
   }
   else{
-    res.send({success:false})
+    console.log(err);
+    //res.send({success:false})
   }
 });
 
@@ -124,31 +131,131 @@ app.get('/drivesearch', function (req, res) {
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listFiles(auth) {
+
+function getFormattedDate(date) {
+	var date_n = new Date(date);
+	var rfc_date = timestamp(date_n);
+	return rfc_date;
+}
+
+function getNextFormattedDate(date) {
+	var date_n = new Date(date);
+	date_n.setTime( date_n.getTime() + 1 * 86400000 );
+	 var rfc_date = timestamp(date_n);
+        return rfc_date;
+}
+
+function process_response(auth,data,res) {
+	var query_str = "";
+	data = data["entities"];
+	if(data.hasOwnProperty("search_query")){
+		var queries = data["search_query"];
+		for(var i=0;i< queries.length; i++) {
+			var str = queries[i]["value"];
+			if(query_str!="") {
+					query_str += "or ";
+			}
+			query_str += "fulltext contains \'" + str + "\' ";
+			query_str += "or name contains \'" + str + "\' ";
+		}
+	}
+	if(data.hasOwnProperty("datetime")){
+                var queries = data["datetime"];
+                for(var i=0;i< queries.length; i++) {
+                        var str = queries[i]["value"];
+                        if(query_str!="") {
+                                        query_str += "or (";
+                        }
+                        query_str += "modifiedTime >= \'" + getFormattedDate(str) + "\' ";
+			query_str += "and modifiedTime < \'" + getNextFormattedDate(str) + "\') ";
+			break;	
+                }
+        }
+	if(data.hasOwnProperty("file_type")){
+                var queries = data["file_type"];
+                for(var i=0;i< queries.length; i++) {
+                        var str = queries[i]["value"];
+                        if(query_str!="") {
+                                        query_str += "or ";
+                        }
+                        query_str += "mimeType contains \'" + str + "\' ";
+                }
+        }
+	if(data.hasOwnProperty("contact")) {
+		var queries = data["contact"];
+                for(var i=0;i< queries.length; i++) {
+                        var str = queries[i]["value"];
+                        if(query_str!="") {
+                                        query_str += "or ";
+                        }
+                        query_str += "\'" + str + "\' in writers ";
+                        query_str += "or \'" + str + "\' in readers ";
+			query_str += "or \'" + str + "\' in ownders ";
+                }
+	}
+	console.log(query_str);
+	getFiles(auth,query_str,res);
+	
+
+
+}
+	
+			
+function getQuery(auth, searchstring, res) {
+	console.log(searchstring)
+	const client = new Wit({accessToken: ACCESS_TOKEN});
+		client.message(searchstring, {})
+.then((data) => {
+	  console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+		process_response(auth,data,res);
+	})
+	.catch(console.error);
+	
+}
+
+
+
+function listFiles(auth,searchstring,res) {
+	getQuery(auth,searchstring,res);
+}
+
+function getFiles(auth,query_str,res) {
   var service = google.drive('v3');
   service.files.list({
     auth: auth,
     pageSize: 100,
     fields: "nextPageToken, files(id, name, owners)",
-    q: "name contains 'CS231n'"
+    q: query_str
   }, function(err, response) {
     if (err) {
       console.log('The API returned an error: ' + err);
+	res.send({"success": false , "error": "Google API returned an error"});
       return;
     }
     var files = response.files;
     if (files.length == 0) {
+	res.send({"success": true, "results": [] });
       console.log('No files found.');
     } else {
+	res.send({"success": true, "results": files});
       console.log('Files:');
       for (var i = 0; i < files.length; i++) {
         var file = files[i];
-        console.log(files)
-        console.log('%s (%s) link: %s', file.name, file.id, file.owners);
+        //console.log(JSON.stringify(files))
+        //console.log('%s (%s) link: %s', file.name, file.id, file.owners);
       }
     }
   });
 }
 
+var server = require('http').Server(app);
 
-app.listen(8080);
+var port1 =  8080;
+server.listen(port1,  process.argv[2] || "0.0.0.0", function (err) {
+    if (err)
+        throw err;
+    else
+        console.log('Socket and Rest calls can be made on port : ' + port1)
+});
+
+//app.listen(8080);
